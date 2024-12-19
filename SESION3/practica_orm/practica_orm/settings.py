f4b043a03e3fb29a37b22e18a6dfa1b4f3037459
Yuri Urzua Lebuy <yuri19762008@hotmail.com>
2024-12-19 12:39:56 -0300

    FIX

--- SESION3/practica_orm/practica_orm/settings.py
+++ SESION3/practica_orm/practica_orm/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'productos',  # Agrega la app productos a la lista de aplicaciones instaladas   
 ]
 
 MIDDLEWARE = [
