1c616e3096ce2d42af185dba82cfece56147dbf8
Yuri Urzua Lebuy <yuri19762008@hotmail.com>
2024-12-31 21:27:52 -0300

    FIX

--- SESION6/practica_orm/productos/admin.py
+++ SESION6/practica_orm/productos/admin.py
@@ -5,7 +5,7 @@ from .models import Producto
 
 @admin.register(Producto)
 class ProductoAdmin(admin.ModelAdmin):
-    list_display = ('id', 'nombre', 'descripcion', 'precio', 'fabricante')
-    list_filter = ('precio',)
+    list_display = ('id', 'nombre', 'descripcion', 'precio', 'fabricante','pais', 'f_vencimiento')
+    list_filter = ('precio','fabricante')
     search_fields = ('nombre', 'descripcion')
     ordering = ('id',)
