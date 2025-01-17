7b4a0f014f54c8260316f3acae7e25ce13f9f5ae
Yuri Urzua Lebuy <yuri19762008@hotmail.com>
2025-01-17 20:44:20 -0300

    Final

--- FINAL/final_farmacia/config/laboratorio/admin.py
+++ FINAL/final_farmacia/config/laboratorio/admin.py
@@ -3,12 +3,15 @@ from .models import Laboratorio, DirectorGeneral, Producto
 
 @admin.register(Laboratorio)
 class LaboratorioAdmin(admin.ModelAdmin):
-    list_display = ('nombre', 'ciudad', 'pais')
+    list_display = ('id', 'nombre', 'ciudad', 'pais')
+    ordering = ('id',)
 
 @admin.register(DirectorGeneral)
 class DirectorGeneralAdmin(admin.ModelAdmin):
-    list_display = ('nombre', 'laboratorio', 'especialidad')
+    list_display = ('id', 'nombre', 'laboratorio')
+    ordering = ('id',)
 
 @admin.register(Producto)
 class ProductoAdmin(admin.ModelAdmin):
-    list_display = ('nombre', 'laboratorio', 'f_fabricacion', 'p_costo', 'p_venta')
+    list_display = ('id', 'nombre', 'laboratorio', 'f_fabricacion', 'p_costo', 'p_venta')
+    ordering = ('id',)
